5e14c233da47fc44b45d59b50c8aa9719a287a55
ASUS <ASUS@DESKTOP-J2K4H6B>
2022-05-07 11:00:40 +0700

    Add project files.

---
+++ DemoASM/Areas/Identity/Data/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DemoASM.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AppUser class
+public class AppUser : IdentityUser
+{
+    public string? FullName { get; set; }
+    public string? Address { get; set; }
+    public DateTime? DoB { get; set; }
+    public string? Gender { get; set;}
+}
---
+++ DemoASM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoASM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
