cb0aef3ed1f13c3047b69fc2be2b195f7ddf674d
Khánh Nguyễn <hanaaimi304@gmail.com>
2022-05-10 08:42:41 +0700

    carr and order ctl

--- DemoASM/Controllers/OrdersController.cs
+++ DemoASM/Controllers/OrdersController.cs
@@ -8,42 +8,81 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DemoASM.Data;
 using DemoASM.Models;
+using System.Security.Claims;
 
 namespace DemoASM.Controllers
 {
     public class OrdersController : Controller
     {
         private readonly DemoASMContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public OrdersController(DemoASMContext context)
+        public OrdersController(DemoASMContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         // GET: Orders
         public async Task<IActionResult> CheckOut()
         {
-            var demoASMContext = _context.Orders.Include(o => o.User);
-            return View(await demoASMContext.ToListAsync());
+            var currentId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            List<Cart> myCartDetails = await _context.Carts
+                .Where(c => c.UserId == currentId)
+                .Include(c => c.IsbnNavigation).ToListAsync();
+            return View(myCartDetails);
         }
 
         // GET: Orders/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Buy(int? id)
         {
-            if (id == null)
+            var currentId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            List<Cart> myCartDetails = await _context.Carts
+                .Where(c => c.UserId == currentId)
+                .Include(c => c.IsbnNavigation)
+                .ToListAsync();
+            using (var transaction = _context.Database.BeginTransaction())
             {
-                return NotFound();
-            }
+                try
+                {
+                    //Step 1: create an order
+                    Order myOrder = new Order();
+                    myOrder.UserId = currentId;
+                    myOrder.OrderDate = DateTime.Now;
+                    double? total = 0;
+                    foreach (Cart cart in myCartDetails)
+                    {
+                        total += cart.IsbnNavigation.Price * cart.Quantity;
+                    }
+                    myOrder.TotalPrice = total;
+                    _context.Add(myOrder);
+                    await _context.SaveChangesAsync();
 
-            var order = await _context.Orders
-                .Include(o => o.User)
-                .FirstOrDefaultAsync(m => m.OrderId == id);
-            if (order == null)
-            {
-                return NotFound();
-            }
+                    //Step 2: insert all order details by var "myDetailsInCart"
+                    foreach (var item in myCartDetails)
+                    {
+                        OrderDetail detail = new OrderDetail()
+                        {
+                            OrderId = myOrder.OrderId,
+                            Isbn = item.Isbn,
+                            Quantity = item.Quantity,
+                        };
+                        _context.Add(detail);
+                    }
+                    await _context.SaveChangesAsync();
 
-            return View(order);
+                    //Step 3: empty/delete the cart we just done for thisUser
+                    _context.Carts.RemoveRange(myCartDetails);
+                    await _context.SaveChangesAsync();
+                    transaction.Commit();
+                }
+                catch (DbUpdateException ex)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine("Error occurred in Checkout" + ex);
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         // GET: Orders/Create
---
+++ DemoASM/Migrations/DemoASM/20220510011923_testcode1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoASM.Migrations.DemoASM
+{
+    public partial class testcode1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
